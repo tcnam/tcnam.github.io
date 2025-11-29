bef9c86a42dfdae9c7bf77715b5e681db4aa2b02
tcnam <56421023+tcnam@users.noreply.github.com>
2025-11-29 10:03:21 +0700

    update config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tcnam', // Usually your GitHub org/user name.
   projectName: 'book_reviews', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
 
